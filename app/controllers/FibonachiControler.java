1072fb194cd2a7d44abde387b653650d294f1aa6
Slava Osepyan <mr-93@mail.ru>
2017-10-29 18:17:31 +0300

    working controller

--- app/controllers/FibonachiControler.java
+++ app/controllers/FibonachiControler.java
@@ -1,7 +1,29 @@
 package controllers;
 
+import play.mvc.Controller;
+import play.mvc.Result;
+import services.IntermediaryFibo;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
 /**
  * @author v.osepyan
  */
-public class FibonachiControler {
+
+@Singleton
+public class FibonachiControler extends Controller{
+
+    private final IntermediaryFibo intermediaryFibo;
+
+    @Inject
+    public FibonachiControler (IntermediaryFibo intermediaryFibo){
+        this.intermediaryFibo = intermediaryFibo;
+    }
+
+    public Result count(int n) {
+        return ok(intermediaryFibo.getFibonachiSequence(n));
+    }
+
+
 }
